71a4e6cb23ee02970ee9df5825b5d6ec218a4d9b
whalenailer <30739567+whalenailer@users.noreply.github.com>
2017-08-05 20:27:06 -0500

    initial

---
+++ ConsoleApp1/MyBot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord;
+using Discord.Commands;
+
+namespace ConsoleApp1
+{
+    class MyBot
+    {
+        DiscordClient discord;
+
+        public MyBot()
+        {
+            Discord = new discordclient(x =>
+            {
+                x.loglevel = logseverity.info;
+                x.loghandler = log;
+            });
+
+
+            discord.ExecuteAndWait(async() =>
+            {
+                await discord.connect("MzQzNDM5NzA1OTI2NTMzMTIx.DGeNgg.ZLPFwcqNikeQ2wUxlar1CPwMG9U");
+            });
+        } 
+    private void log(object sender, LogMessageEventArgs)
+    {
+        Console.WriteLine(e.message);
+    }
+}
+
+}
